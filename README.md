cb4ea01e1de267239e5f26ec03e0cda1e8b39c6a
Monika <mwjung10@gmail.com>
2024-03-19 14:47:33 +0100

    readme edited

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 https://prod.liveshare.vsengsaas.visualstudio.com/join?592F5F94DF3927A19EEAFF282508B5E807D2
 dobra ksiazka: http://neuralnetworksanddeeplearning.com/chap1.html
+
+MLP explained: https://github.com/KirillShmilovich/MLP-Neural-Network-From-Scratch/blob/master/MLP.ipynb
--- first_implementation.py
+++ first_implementation.py
@@ -9,9 +9,9 @@ from sklearn.metrics import accuracy_score
 # Load the digits dataset
 digits = datasets.load_digits(as_frame=True)
 
-# Display the last digit
+# Display digit
 # plt.figure(1, figsize=(3, 3))
-# plt.imshow(digits.images[0], cmap='binary')
+# plt.imshow(digits.images[70], cmap='binary')
 # plt.show()
 
 print(type(digits))
@@ -20,16 +20,16 @@ n_samples, n_features = digits.data.shape
 print((n_samples, n_features))
 X = digits.data
 y = digits.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
 
-mlp = MLPClassifier(hidden_layer_sizes=(5,),  # number of neurons in the hidden layer
+mlp = MLPClassifier(hidden_layer_sizes=(40),  # number of neurons in the hidden layer
                     activation='logistic',   # activation function (logistic sigmoid)
                     alpha=1e-4,     # helps preventing overfittting
                     solver='sgd',    # optimization algorithm
                     tol=1e-4,       # tolerance for optimzation; determines when to stop it
                     random_state=1,  #  This parameter sets the random seed for reproducibility. By setting it to 1, the results will be reproducible across multiple runs.
-                    learning_rate_init=.3, # learning rate for the optimization algorithm. It determines the size of the steps
+                    learning_rate_init=.1, # learning rate for the optimization algorithm. It determines the size of the steps
                     verbose=True)  # print progress messages to the console during training. Setting it to True enables verbose output.
 mlp.fit(X_train, y_train)
 preds = mlp.predict(X_test)
